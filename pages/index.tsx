bfb9f8b21cb16d48b5c723b66d029e8a18c35dce
Andriy Shcherbyak <>
2022-04-23 19:33:23 +0200

    chore: test pr

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,6 @@ const Home: NextPage = () => {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
